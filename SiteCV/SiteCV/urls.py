a16e4c33e2b1955b22b2fd137d2b3cd14a6d2d0b
alexfreyjr <alexfreyjr@gmail.com>
2022-04-28 11:30:49 +0200

    first test page

--- SiteCV/SiteCV/urls.py
+++ SiteCV/SiteCV/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from skills import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('skills/', views.skills),
+    path('experiences/', views.experiences),
+    path('cursus/', views.cursus),
 ]
